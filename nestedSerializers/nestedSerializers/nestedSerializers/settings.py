623dddd102f706e84d06b7ab5a393361d396ceb1
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-04 17:46:53 +0530

    DRF Global Security

--- nestedSerializers/nestedSerializers/nestedSerializers/settings.py
+++ nestedSerializers/nestedSerializers/nestedSerializers/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     'nsApp',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':['rest_framework.authentication.BasicAuthentication'],
+    'DEFAULT_PERMISSION_CLASSES':['rest_framework.permissions.IsAuthenticated','rest_framework.permissions.DjangoModelPermissions']
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- nestedSerializers/nestedSerializers/nsApp/views.py
+++ nestedSerializers/nestedSerializers/nsApp/views.py
@@ -1,14 +1,11 @@
 from nsApp.serializers import BookSerializer,AuthorSerializer
 from rest_framework import generics
 from nsApp.models import Author,Book
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
 class list_author(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
 class deatis_author(generics.RetrieveUpdateDestroyAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
